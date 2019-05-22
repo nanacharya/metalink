42edb9d7333d9498c00b879726dd8e3ed3bcc963
GHASJ <ghasj@nscorp.com>
2019-05-21 23:22:12 -0400

    adding email validator

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ml-footer',
@@ -12,7 +12,7 @@ showform:boolean;
 
   form= new FormGroup({
   'name':new FormControl("",[]),
-  'email':new FormControl("",[]),
+  'email':new FormControl("",[Validators.required,Validators.email]),
   'subject':new FormControl("",[]),
   'message':new FormControl("",[]),
 
@@ -25,7 +25,10 @@ showform:boolean;
     this.showform=false;
   }
 
-
+get email(){
+  debugger;
+  return this.form.get("email");
+}
   submitForm(){
     console.log(this.form.value)
   }
